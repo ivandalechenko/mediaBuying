c9eb97dc8ea54fbf646e5e38a309342e3fa010ff
unknown <ivandalechenko@gmail.com>
2024-04-18 02:45:35 +0300

    adding swiper

--- main.js
+++ main.js
@@ -1,1 +1,68 @@
 import './style.scss'
+
+
+import Swiper from 'swiper';
+import 'swiper/css';
+// import 'swiper/scss/navigation';
+// import 'swiper/scss/autoplay';
+
+// import { Navigation, Autoplay } from 'swiper/modules';
+
+setTimeout(() => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: "auto",
+        slidesPerGroup: 1,
+        centeredSlides: true,
+        spaceBetween: 24,
+        loop: true,
+    });
+}, 500);
+
+// const clients = 
+
+const addSlide = (slide) => {
+    const sourceInnerHTML = document.getElementById('clientSlider').innerHTML;
+    const newInnerHTML = sourceInnerHTML + `
+    <div class="clients_slide_wrapper swiper-slide">
+    <div class="clients_slide">
+      <div class="clients_slide_header" style="background:${slide.bg};">
+        <img src="/img/cases/${slide.img}.png" alt="">
+        ${slide.name}
+      </div>
+      <div class="clients_slide_info">
+        <div class="clients_slide_info_header">
+          Tools used:
+        </div>
+        <div class="clients_slide_info_text">
+          ${slide.tools}
+        </div>
+      </div>
+      <div class="clients_slide_info">
+        <div class="clients_slide_info_header">
+          Goal:
+        </div>
+        <div class="clients_slide_info_text">
+          ${slide.goal}
+        </div>
+      </div>
+      <div class="clients_slide_info">
+        <div class="clients_slide_info_header">
+          Period
+        </div>
+        <div class="clients_slide_info_text">
+          ${slide.period}
+        </div>
+      </div>
+      <div class="clients_slide_info">
+        <div class="clients_slide_info_header clients_slide_info_header_results">
+          Results
+        </div>
+        <div class="clients_slide_info_text">
+          ${slide.result}
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    document.getElementById('clientSlider').innerHTML = newInnerHTML;
+}
