f7aebfdb3ca82d895346eb8f0ba407090ecdddc1
unknown <ivandalechenko@gmail.com>
2024-04-18 17:51:16 +0300

    Full func no anim

---
+++ clients.js
@@ -0,0 +1,126 @@
+
+const clients = [
+  {
+    name: 'MTS MUSIC',
+    bg: '#8120FE',
+    img: 'mts.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Utility"
+  },
+  {
+    name: 'MELBET',
+    bg: '#FEBA02',
+    img: 'mb.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Betting"
+  },
+  {
+    name: 'SURFSHARK',
+    bg: '#1EC0C0',
+    img: 'surfshark.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Utility"
+  },
+  {
+    name: 'PsychicBook',
+    bg: 'linear-gradient(to right, #009273, #02604B);',
+    img: 'phychicBook.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Utility"
+  },
+  {
+    name: 'FAVBET',
+    bg: '#FF267D',
+    img: 'favbet.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Betting"
+  },
+  {
+    name: 'Cosmolot',
+    bg: 'linear-gradient(to right, #C80E7B, #3E3AD2)',
+    img: 'cosmolot.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Casino"
+  },
+  {
+    name: 'DIGIDO',
+    bg: '#FF2979',
+    img: 'digido.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Utility"
+  },
+  {
+    name: 'ZAIMER',
+    bg: '#3B8C3F',
+    img: 'zaimer.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Utility"
+  },
+  {
+    name: '1XBET',
+    bg: '#3E9BF4',
+    img: '1xbet.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Betting"
+  },
+  {
+    name: '22BET',
+    bg: '#047887',
+    img: '22bet.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Betting"
+  },
+  {
+    name: 'XM TRADING',
+    bg: '#DB0505',
+    img: 'xmtrading.png',
+    tools: 'DSP, Huawei, Xiaomi',
+    goal: 'Acquire In-App first-order users in the mobile',
+    period: '2 months',
+    result: `Over 600 first orders monthly in line with the client's KPI;
+      Optimization based on the metrics of the integrated anti- fraud system Adjust`,
+    cat: "Trading"
+  }
+]
+
+export default clients
---
+++ sliderTools.js
@@ -0,0 +1,89 @@
+import clients from './clients'
+const addSlide = (slide) => {
+  const sourceInnerHTML = document.getElementById('clientSlider').innerHTML;
+  const newInnerHTML = sourceInnerHTML + `
+      <div class="clients_slide_wrapper swiper-slide">
+      <div class="clients_slide">
+        <div class="clients_slide_header" style="background:${slide.bg};">
+          <img src="/img/cases/${slide.img}" class="clients_slide_header_img" alt="">
+          ${slide.name}
+        </div>
+        <div class="clients_slide_info">
+          <div class="clients_slide_info_header">
+            Tools used:
+          </div>
+          <div class="clients_slide_info_text">
+            ${slide.tools}
+          </div>
+        </div>
+        <div class="clients_slide_info">
+          <div class="clients_slide_info_header">
+            Goal:
+          </div>
+          <div class="clients_slide_info_text">
+            ${slide.goal}
+          </div>
+        </div>
+        <div class="clients_slide_info">
+          <div class="clients_slide_info_header">
+            Period
+          </div>
+          <div class="clients_slide_info_text">
+            ${slide.period}
+          </div>
+        </div>
+        <div class="clients_slide_info">
+          <div class="clients_slide_info_header clients_slide_info_header_results">
+            Results
+          </div>
+          <div class="clients_slide_info_text">
+            ${slide.result}
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+  document.getElementById('clientSlider').innerHTML = newInnerHTML;
+}
+const initSlider = () => {
+  for (const client of clients) {
+    addSlide(client)
+  }
+  const allCats = clients.map(client => client.cat);
+  const uniqueCats = allCats.filter((cat, index, self) => self.indexOf(cat) === index);
+
+  let controlsInner = `<div class="clients_controls_element clients_controls_element_selected">All</div>`;
+  for (const uniqueCat of uniqueCats) {
+    controlsInner = controlsInner + `<div class="clients_controls_element ">${uniqueCat}</div>`;
+  }
+  document.getElementById('clientContolList').innerHTML = controlsInner;
+  setTimeout(() => {
+    const conrolElements = document.getElementsByClassName('clients_controls_element');
+    for (let i = 0; i < conrolElements.length; i++) {
+      conrolElements[i].onclick = () => {
+        for (let j = 0; j < conrolElements.length; j++) {
+          conrolElements[j].classList.remove('clients_controls_element_selected')
+        }
+        conrolElements[i].classList.add('clients_controls_element_selected')
+        selectCategory(conrolElements[i].innerHTML)
+      }
+    }
+  }, 100);
+}
+
+const selectCategory = (cat) => {
+  document.getElementById('clientSlider').innerHTML = '';
+  if (cat == 'All') {
+    for (const client of clients) {
+      addSlide(client)
+    }
+  } else {
+    const sortedArray = clients.filter(client => client.cat === cat);
+    for (const client of sortedArray) {
+      addSlide(client)
+    }
+  }
+}
+
+
+export default initSlider()
